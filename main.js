8021bb435ca3a581c0646675c8090f7289ecfc52
Garina <garina.leon@gmail.com>
2024-05-14 15:52:16 +0200

    upgrade main functionalities 14052024

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const http = require("http");
+const exchangeScrap = require("./exchange-scrap");
 
 const port = "3737";
 
-const url_to_scrap = "https://mcb.mg";
+const exchanges = exchangeScrap.getExchanges();
 
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-type": "application/json" });
-  res.end(JSON.stringify({ res: "ok" }));
+const server = http.createServer(async function (req, res) {
+  const url = req.url;
+  if (url == "/exchanges") {
+    const todaysDate = new Date().toISOString().split("T")[0];
+    const data =
+      todaysDate === exchanges.date
+        ? exchanges
+        : await exchangeScrap.getExchanges();
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(JSON.stringify(data));
+  } else {
+    res.writeHead(204, { "Content-type": "application/json" });
+    res.end(JSON.stringify({}));
+  }
 });
 
 server.listen(port);
